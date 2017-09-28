b2537099631b5ce6a746ed4caacc0b0d265ca457
crone66 <marcel.croonenbroeck@gmail.com>
2017-09-28 15:29:27 +0200

    added http config

---
+++ NetworkFramework.HttpExample/Helpers/HttpConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace NetworkFramework.HttpExample
+{
+    public struct HttpConfig
+    {
+        public string LocalIP;
+        public int Port;
+        public string NotFoundPath; //404
+        public string NotAllowedPath; //405
+        public string DefaultCharset;
+        public string RootDir;
+
+        public HttpConfig(string localIP, int port, string notFoundPath, string notAllowedPath, string defaultCharset, string rootDir)
+        {
+            LocalIP = localIP;
+            Port = port;
+            NotFoundPath = notFoundPath;
+            NotAllowedPath = notAllowedPath;
+            DefaultCharset = defaultCharset;
+            RootDir = rootDir;
+        }
+    }
+}
